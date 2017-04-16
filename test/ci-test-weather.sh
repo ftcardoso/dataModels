c8944d9f483273ae23033e16e2cdcb70e41dc66b
Federico M. Facca <federico.facca@gmail.com>
2017-04-16 09:30:43 +0200

    Include more documentation on how to contribute and usage of continuous integration tests.

--- test/ci-test-weather.sh
+++ test/ci-test-weather.sh
@@ -3,8 +3,11 @@
 # Tests the Weather datamodels
 
 testWeatherObserved(){
+   # 1. check that the JSON Schema is a valid JSON Schema
    result=`ajv compile --v5 -s Weather/WeatherObserved/schema.json -r common-schema.json -r geometry-schema.json -r Weather/weather-schema.json`
    assertEquals "schema Weather/WeatherObserved/schema.json is valid" "${result}"
+
+   # 2. check that the JSON example is valid according to the JSON Schema
    result=`ajv test --v5 -s Weather/WeatherObserved/schema.json -r common-schema.json -r geometry-schema.json -r Weather/weather-schema.json -d Weather/WeatherObserved/example.json --valid`
    assertEquals "Weather/WeatherObserved/example.json passed test" "${result}"
 }
